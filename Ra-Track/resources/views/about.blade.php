229ce3c0b00f344fd9d70037eb02f91cdbd50278
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 23:54:09 +0000

    intégration du header dans la page about

---
+++ Ra-Track/resources/views/about.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Agencies - LimoWide Partnership</title>
+</head>
+<body class="bg-gray-50">
+
+    <!-- Navigation -->
+    <div class="hero-section text-white h-screen">
+
+        @include('layouts.header')
+        <!-- Hero Content -->
+        <div class="container mx-auto px-6 py-32 text-center">
+            <h1 class="text-4xl md:text-5xl font-bold mb-4">
+                <span class="text-white">TRAVEL</span>
+                <span class="text-yellow-500">AGENCIES</span>
+            </h1>
+            <p class="text-lg max-w-2xl mx-auto mb-8">
+                Choose your travel experience and let your customers enjoy premium chauffeur services worldwide
+            </p>
+        </div>
+        <div class="attribution">
+            Photo by <a href="https://unsplash.com/@jrkorpa?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">jr Korpa</a> on <a href="https://unsplash.com/photos/a-view-of-a-city-at-night-from-a-mountain-top-91e8fad9978e?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </div>
+    </div>
+
+   
+   
+</body>
+</html>
