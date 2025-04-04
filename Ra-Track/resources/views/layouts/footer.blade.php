7f2b3057ee01d54c0a1690638f707fa2dd24e0a4
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 21:36:05 +0000

    l'ajout de la partie logo & socials row dans le footer

--- Ra-Track/resources/views/layouts/footer.blade.php
+++ Ra-Track/resources/views/layouts/footer.blade.php
@@ -1,5 +1,17 @@
 <footer class="text-gray-300 py-4" style="border-radius:50px 50px 0 0; background-color:#162238;">
         <div class="container mx-auto px-4">
-           
+            <!-- Logo & Socials Row -->
+            <div class="flex flex-col md:flex-row items-center justify-between mx-12 mb-6">
+                <img src="/placeholder.svg?height=60&width=225" alt="LimoWide Logo" class="h-16">
+                <div class="flex space-x-3">
+                    <a href="#" class="hover:text-white" style="color:#FFD476"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#" class="hover:text-white" style="color:#FFD476"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="hover:text-white" style="color:#FFD476"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="hover:text-white" style="color:#FFD476"><i class="fab fa-pinterest"></i></a>
+                    <a href="#" class="hover:text-white" style="color:#FFD476"><i class="fab fa-whatsapp"></i></a>
+                </div>
+            </div>
+
+            
         </div>
 </footer>
