c91080815b75eaabefc2128c085f7cd080ae79d6
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 21:32:01 +0000

    l'ajout des liens necessaires pour une navigation entre les pages

--- Ra-Track/resources/views/layouts/header.blade.php
+++ Ra-Track/resources/views/layouts/header.blade.php
@@ -5,7 +5,15 @@
             <a href="#" class="text-2xl font-bold">LIMOWIDE</a>
             <nav>
                 <ul class="flex items-center space-x-8">
-                    
+                <li><a href="/" class="hover:text-gray-300">Home</a></li>
+                    <li><a href="/about" class="hover:text-gray-300">About us</a></li>
+                    <li><a href="/services" class="hover:text-gray-300">Services</a></li>
+                    <li><a id="openRegisterModalBtn"  href="/register" class="hover:text-gray-300" >Register</a></li>
+                    <li><a href="/login">
+                        <button class="text-black px-6 py-2 rounded-full hover:bg-yellow-600" style="background-color:#FFD476">Sign
+                             In
+                        </button>
+                    </a></li>
                 </ul>
             </nav>
         </div>
