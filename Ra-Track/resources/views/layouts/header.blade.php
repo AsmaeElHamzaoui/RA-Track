4c7955f491276c8a1f87e00ec22123c5d6ade96a
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 21:29:32 +0000

    la construction du squelete html du header

---
+++ Ra-Track/resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<!-- Header -->
+<div class="header-container">
+    <header class="text-white py-4">
+        <div class="container mx-auto flex items-center justify-between px-4">
+            <a href="#" class="text-2xl font-bold">LIMOWIDE</a>
+            
+        </div>
+    </header>
+</div>
+
