1f77e2f1f84bedbc8f7219286849f5c9704c3544
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 21:30:41 +0000

    l'ajout des élements du navbar dans le header

--- Ra-Track/resources/views/layouts/header.blade.php
+++ Ra-Track/resources/views/layouts/header.blade.php
@@ -3,7 +3,11 @@
     <header class="text-white py-4">
         <div class="container mx-auto flex items-center justify-between px-4">
             <a href="#" class="text-2xl font-bold">LIMOWIDE</a>
-            
+            <nav>
+                <ul class="flex items-center space-x-8">
+                    
+                </ul>
+            </nav>
         </div>
     </header>
 </div>
