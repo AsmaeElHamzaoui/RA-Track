fb739d2901a25f3d294e20c51263d2d5ce1ee167
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 21:34:57 +0000

    la construction du squelete html du footer

---
+++ Ra-Track/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-gray-300 py-4" style="border-radius:50px 50px 0 0; background-color:#162238;">
+        <div class="container mx-auto px-4">
+           
+        </div>
+</footer>
