326f6d48218fa0b65897b90377527eaf8e86f817
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 23:58:15 +0000

    l'ajout des liens CDN pour les icons et le framework tailwind

--- Ra-Track/resources/views/about.blade.php
+++ Ra-Track/resources/views/about.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Travel Agencies - LimoWide Partnership</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
         body {
             font-family: 'Montserrat', sans-serif;
