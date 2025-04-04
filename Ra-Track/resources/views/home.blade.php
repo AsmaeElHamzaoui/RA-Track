ee0787eded859583f8439ac5f26af893baed2d24
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 23:03:44 +0000

    intégration des liens necessaires dans le head

--- Ra-Track/resources/views/home.blade.php
+++ Ra-Track/resources/views/home.blade.php
@@ -5,6 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LimoWide</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'navy': {
+                            800: '#1a2e4c',
+                        },
+                    },
+                }
+            }
+        }
+    </script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    
 </head>
 
 <body class="bg-gray-100">
