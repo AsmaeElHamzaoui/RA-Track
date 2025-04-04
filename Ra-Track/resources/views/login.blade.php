a8b2eda0efd6f4b43103031ed86ac7598251a855
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-04 23:24:25 +0000

    l'ajout de la deuxième section du formulaire

--- Ra-Track/resources/views/login.blade.php
+++ Ra-Track/resources/views/login.blade.php
@@ -11,7 +11,30 @@
 
     <div class="register-container">
 
-       
+        <!-- Formulaire de connexion (à gauche) -->
+        <form class="register-form" action="#" method="POST">
+            <h2>Login</h2>
+            <div class="inputBox">
+                <input type="email" name="email" required="required">
+                <i class="fa-regular fa-envelope"></i>
+                <span>Email Address</span>
+            </div>
+            <div class="inputBox">
+                <input type="password" name="password" required="required">
+                <i class="fa-solid fa-lock"></i>
+                <span>Password</span>
+            </div>
+            <div class="inputBox">
+                <input type="submit" value="Login">
+            </div>
+            <p>Not a member? <a href="/register">Create Account</a></p>
+        </form>
+
+        <!-- Section du Logo et Texte (à droite) -->
+        <div class="logo-section">
+            <img src="URL_DE_VOTRE_LOGO" alt="Logo LimoWide"> <!-- Remplacez par l'URL de votre logo -->
+            <p>Bienvenue chez LimoWide. Connectez-vous pour accéder à votre compte!</p>
+        </div>
     </div>
 
 </body>
