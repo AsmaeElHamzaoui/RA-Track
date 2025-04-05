a5a671ec442f5ddfca8bb66956712cda87efdad6
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:11:48 +0000

    implémentation de la relation one to many entre l'entité Reservation et l'entité User

--- Ra-Track/app/Models/Reservation.php
+++ Ra-Track/app/Models/Reservation.php
@@ -15,5 +15,14 @@ class Reservation extends Model
         'class', // Classe choisie (économy, business, first)
     ];
 
-  
+     /**
+     * Relation N:1 avec l'entité User
+     * Une réservation appartient à un utilisateur.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    
 }
