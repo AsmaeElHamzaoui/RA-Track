dc762e6325a986d1522be8d8b7001d7543012f1d
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 12:50:34 +0000

    l'ajout des colonne necessaires à la migration flights

--- Ra-Track/database/migrations/2025_04_02_234659_create_flights_table.php
+++ Ra-Track/database/migrations/2025_04_02_234659_create_flights_table.php
@@ -7,7 +7,20 @@ class CreateFlightsTable extends Migration
 {
     public function up()
     {
-       
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('flight_number')->unique(); // Flight number (unique)
+            $table->foreignId('plane_id')->constrained('planes')->onDelete('cascade'); // Assigned plane to the flight
+            $table->foreignId('departure_airport_id')->constrained('airports')->onDelete('cascade'); // Departure airport
+            $table->foreignId('arrival_airport_id')->constrained('airports')->onDelete('cascade'); // Arrival airport
+            $table->dateTime('departure_time'); // Scheduled departure time
+            $table->dateTime('arrival_time'); // Scheduled arrival time
+            $table->enum('status', ['scheduled', 'in_progress', 'cancelled', 'delayed', 'completed']); // Flight status :['programmé', 'en cours', 'annulé', 'retardé', 'terminé']);
+            $table->float('economy_class_price'); // Economy class price
+            $table->float('business_class_price'); // Business class price
+            $table->float('first_class_price'); // First class price
+            $table->timestamps();
+        });
     }
 
     public function down()
