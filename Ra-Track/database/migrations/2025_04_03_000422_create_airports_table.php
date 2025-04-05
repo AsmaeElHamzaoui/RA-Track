652d93c63fe677b55a6e5792994899fecf7871b1
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 12:49:20 +0000

    l'ajout des colonne necessaires à la migration airports

--- Ra-Track/database/migrations/2025_04_03_000422_create_airports_table.php
+++ Ra-Track/database/migrations/2025_04_03_000422_create_airports_table.php
@@ -8,7 +8,13 @@ class CreateAirportsTable extends Migration
 {
     public function up()
     {
-       
+        Schema::create('airports', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_iata')->unique(); // Code IATA de l'aéroport
+            $table->string('name'); // Nom de l'aéroport
+            $table->string('location'); // Emplacement de l'aéroport
+            $table->timestamps();
+        });
     }
 
     public function down()
