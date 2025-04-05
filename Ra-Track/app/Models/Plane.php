084e6174b9bd6aa4fa490810ba2b343f961650b1
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:08:55 +0000

    implémentation de la relation one to many entre l'entité Plane et l'entité FlightTracking

--- Ra-Track/app/Models/Plane.php
+++ Ra-Track/app/Models/Plane.php
@@ -31,6 +31,13 @@ class Plane extends Model
         return $this->hasMany(Flight::class, 'plane_id');
     }
 
-   
+     /**
+     * Relation 1:N avec l'entité FlightTracking (Suivi des vols)
+     * Un avion peut être lié à plusieurs suivis de vols.
+     */
+    public function flightTrackings()
+    {
+        return $this->hasMany(FlightTracking::class, 'plane_id');
+    }
 }
 
