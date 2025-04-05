1387487f2f94c02600387b775dc97117d326536c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:32:55 +0000

    implémentation de la fonction register

--- Ra-Track/app/Http/Controllers/AuthController.php
+++ Ra-Track/app/Http/Controllers/AuthController.php
@@ -11,5 +11,35 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    
+    /**
+     * Inscription d'un nouvel utilisateur
+     */
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'role' => 'in:admin,maintenanceagent,pilot,client', // Vérification du rôle
+        ]);
+
+        // Création de l'utilisateur
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => $validatedData['role'] ?? 'client', // Par défaut, rôle client
+        ]);
+
+        // Création du token d'authentification
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+  
 }
