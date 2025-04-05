0760ba444376ccfd0ce62a8e5aa6373b5571e721
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 14:04:59 +0000

    implémentation de la fonction store dans le controleur Flight

--- Ra-Track/app/Http/Controllers/FlightController.php
+++ Ra-Track/app/Http/Controllers/FlightController.php
@@ -16,4 +16,32 @@ class FlightController extends Controller
         return response()->json($flights);
     }
 
+    
+    /**
+     * Enregistrer un nouveau vol.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'flight_number' => 'required|string|unique:flights',
+            'plane_id' => 'required|exists:planes,id',
+            'departure_airport_id' => 'required|exists:airports,id',
+            'arrival_airport_id' => 'required|exists:airports,id|different:departure_airport_id',
+            'departure_time' => 'required|date|after:now',
+            'arrival_time' => 'required|date|after:departure_time',
+            'status' => 'required|string|in:scheduled,in_progress,cancelled,delayed,completed',
+            'economy_class_price' => 'required|numeric|min:0',
+            'business_class_price' => 'required|numeric|min:0',
+            'first_class_price' => 'required|numeric|min:0',
+        ]);
+
+        $flight = Flight::create($request->all());
+
+        return response()->json([
+            'message' => 'Flight created successfully',
+            'flight' => $flight
+        ], 201);
+    }
+
+  
 }
