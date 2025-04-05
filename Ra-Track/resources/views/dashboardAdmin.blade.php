7b71dd746a087b1ddec46d660af7167be7f6e3fd
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 12:14:20 +0000

    La création du section Live Flight Map dans la section dashboard

--- Ra-Track/resources/views/dashboardAdmin.blade.php
+++ Ra-Track/resources/views/dashboardAdmin.blade.php
@@ -112,7 +112,24 @@
                 </div>
             </section>
 
-            
+             <!-- Live Flight Map -->
+             <section id="live-map-section" class="bg-navy-light p-4 rounded-lg shadow-md mb-6">
+                 <div class="flex justify-between items-center mb-4">
+                    <h3 class="text-lg font-semibold">Carte des Vols en Direct</h3>
+                    <div class="flex space-x-2">
+                        <button class="flex items-center space-x-1 px-3 py-1 bg-gray-600 hover:bg-gray-500 text-xs rounded">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" /></svg>
+                            <span>Filtrer</span>
+                        </button>
+                         <button class="flex items-center space-x-1 px-3 py-1 bg-gray-600 hover:bg-gray-500 text-xs rounded">
+                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9M20.25 20.25h-4.5m4.5 0v-4.5m0 4.5L15 15" /></svg>
+                         </button>
+                    </div>
+                </div>
+                <div class="h-64 md:h-96 bg-black rounded map-placeholder">
+                    <!-- Map -->
+                </div>
+            </section>
         </div>
 
         
