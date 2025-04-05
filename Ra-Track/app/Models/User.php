91771f7c424cddb5ca71d1bd1b67c5632af23276
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 12:57:42 +0000

    l'ajout du role dans le fillable du model user

--- Ra-Track/app/Models/User.php
+++ Ra-Track/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,34 +11,56 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Vérifie si l'utilisateur est un administrateur.
+     */
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Vérifie si l'utilisateur est un pilote.
+     */
+    public function isPilot()
+    {
+        return $this->role === 'pilot';
+    }
+
+    /**
+     * Vérifie si l'utilisateur est un agent de maintenance.
+     */
+    public function isMaintenanceAgent()
+    {
+        return $this->role === 'maintenanceagent';
+    }
+
+    /**
+     * Vérifie si l'utilisateur est un client.
+     */
+    public function isClient()
+    {
+        return $this->role === 'client';
+    }
+
+   
+
+
 }
