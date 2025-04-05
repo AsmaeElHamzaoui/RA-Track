246d29dff5b24378ef649017240afcedea7ac3c2
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:02:16 +0000

    l'ajout du Attributs assignables en masse dans le fillable de l'entité Flight

--- Ra-Track/app/Models/Flight.php
+++ Ra-Track/app/Models/Flight.php
@@ -9,5 +9,19 @@ class Flight extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'flight_number',           // Numéro du vol
+        'plane_id',                // ID de l'avion affecté
+        'departure_airport_id',    // ID de l'aéroport de départ
+        'arrival_airport_id',      // ID de l'aéroport d'arrivée
+        'departure_time',          // Heure de départ
+        'arrival_time',            // Heure d'arrivée
+        'status',                  // Statut du vol
+        'economy_class_price',     // Prix classe économique
+        'business_class_price',    // Prix classe business
+        'first_class_price',       // Prix classe première
+    ];
+
+   
     
 }
