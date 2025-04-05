993822149d576f0f8a7056ed15a95371257f28a7
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 14:04:35 +0000

    implémentation de la fonction index dans le controleur Flight

--- Ra-Track/app/Http/Controllers/FlightController.php
+++ Ra-Track/app/Http/Controllers/FlightController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
-   
+    /**
+     * Afficher la liste de tous les vols.
+     */
+    public function index()
+    {
+        $flights = Flight::with(['plane', 'departureAirport', 'arrivalAirport'])->get();
+        return response()->json($flights);
+    }
+
 }
