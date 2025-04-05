bed5c63874790761f18041e171f0e0327d6ba8d1
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 12:46:31 +0000

    l'ajout des colonne necessaires à la migration planes

--- Ra-Track/database/migrations/2025_04_02_221912_create_planes_table.php
+++ Ra-Track/database/migrations/2025_04_02_221912_create_planes_table.php
@@ -7,7 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-       
+        Schema::create('planes', function (Blueprint $table) {
+            $table->uuid('id')->primary();  // Unique identifier for the plane (UUID)
+            $table->string('registration')->unique();  // immatruculation
+            $table->string('model'); // Model of the plane (e.g., Airbus A320)
+            $table->string('manufacturer'); // Manufacturer of the plane (e.g., Airbus, Boeing)
+            $table->string('airline_company'); // Airline company the plane belongs to
+            $table->integer('economy_class_capacity');  // Passenger capacity in economy class
+            $table->integer('business_class_capacity'); // Passenger capacity in business class
+            $table->integer('first_class_capacity'); // Passenger capacity in first class 
+            $table->float('maximum_load'); // la masse maximal (in kilograms)
+            $table->integer('flight_range'); // Maximum flight range of the plane (in kilometers) (distance de vol en Km)
+            $table->enum('status', ['active', 'under maintenance', 'out of service']); // Status of the plane (active, under maintenance, or out of service)
+            $table->timestamps(); // Timestamps for record creation and updates
+        });
     }
 
     public function down(): void
