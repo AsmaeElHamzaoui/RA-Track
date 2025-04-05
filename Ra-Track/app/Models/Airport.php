eeff79884c67b823f0110a3175e0195f52ce6122
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:05:35 +0000

    l'ajout du Attributs assignables en masse dans le fillable de l'entité Airport

--- Ra-Track/app/Models/Airport.php
+++ Ra-Track/app/Models/Airport.php
@@ -9,5 +9,11 @@ class Airport extends Model
 {
     use HasFactory;
 
- 
+    protected $fillable = [
+        'code_iata', // Code IATA de l'aéroport
+        'name',      // Nom de l'aéroport
+        'location',  // Emplacement de l'aéroport
+    ];
+
+   
 }
