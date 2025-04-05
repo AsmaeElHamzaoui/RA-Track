cd6ffec19419a03e259e4fb5f4730f281289486e
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 00:06:54 +0000

    L'implémentation de la section direction

--- Ra-Track/resources/views/services.blade.php
+++ Ra-Track/resources/views/services.blade.php
@@ -34,7 +34,82 @@
     </div>
   </div>
 
+  <div class="container">
+    <div class="main-wrapper">
+
+      <!-- direction -->
+      <section class="featured-posts">
+        <div class="featured-posts-container">
+          <div class="main-post">
+            <img
+              src="https://storage.googleapis.com/jpn-new-wp/uploads/2021/08/ae67a5a6-image_kyoto.jpg"
+              alt="Tokyo Tales" />
+            <div class="post-content">
+              <div class="post-category">Travel</div>
+              <h2 class="post-title">Tokyo Tales: A Journey through Tradition and Innovation</h2>
+              <p class="post-description">
+                Explore over a million flight options with our comprehensive search
+                tool, offering endless possibilities for your next adventure.
+                Explore over a million flight options with our comprehensive
+                search tool, offering endless possibil....
+              </p>
+              <p class="post-author">by Limowide • Nov 20, 2023</p>
+              <a href="#" class="read-more">Read More >></a>
+            </div>
+          </div>
+
+          <div class="right-column">
+            <h2 class="text-2xl font-bold text-gray-800 mb-4">Featured Posts</h2>
+            <div class="featured-posts-grid">
+
+              <div>
+                <img
+                  src="https://hips.hearstapps.com/hmg-prod/images/passenger-airplane-landing-at-dusk-royalty-free-image-1569607365.jpg"
+                  alt="Airport Transfer" />
+                <div class="post-content">
+                  <div class="post-category">Transportation</div>
+                  <h3 class="post-title">
+                    Arrive in Style: Luxury Airport Transfer Experiences
+                  </h3>
+                  <p class="post-date">Nov 20, 2023</p>
+                </div>
+              </div>
+
+              <div>
+                <img
+                  src="https://www.shutterstock.com/image-illustration/business-people-traveling-airplane-airport-260nw-321885935.jpg"
+                  alt="Machu Picchu" />
+                <div class="post-content">
+                  <div class="post-category">Travel</div>
+                  <h3 class="post-title">Machu Picchu Marvels: Exploring Ancient Inca Ruins</h3>
+                  <p class="post-date">Nov 20, 2023</p>
+                </div>
+              </div>
+
+              <div>
+                <img
+                  src="https://www.igms.com/content/images/size/w2000/wordpress/2023/07/shutterstock_1017866470-scaled.jpg"
+                  alt="Booking Flights" />
+                <div class="post-content">
+                  <div class="post-category">Booking</div>
+                  <h3 class="post-title">Skyward Journeys: Insider Tips for Booking Flights</h3>
+                  <p class="post-date">Nov 20, 2023</p>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
+
+    </div>
+  </div>
+
  
+
 </body>
 
 </html>
