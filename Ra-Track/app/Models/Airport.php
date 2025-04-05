a4c1e90d13499933018168b1ad9155d6eea0c0fb
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-05 13:06:13 +0000

    implémentation de la relation one to many entre l'entité Airport et l'entité Flight

--- Ra-Track/app/Models/Airport.php
+++ Ra-Track/app/Models/Airport.php
@@ -15,5 +15,21 @@ class Airport extends Model
         'location',  // Emplacement de l'aéroport
     ];
 
-   
+    /**
+     * Relation 1:N avec Flight (Aéroport de départ)
+     * Un aéroport peut être l'aéroport de départ de plusieurs vols.
+     */
+    public function departures()
+    {
+        return $this->hasMany(Flight::class, 'departure_airport_id');
+    }
+
+    /**
+     * Relation 1:N avec Flight (Aéroport d'arrivée)
+     * Un aéroport peut être l'aéroport d'arrivée de plusieurs vols.
+     */
+    public function arrivals()
+    {
+        return $this->hasMany(Flight::class, 'arrival_airport_id');
+    }
 }
