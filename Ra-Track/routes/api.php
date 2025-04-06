6cfb61a3ba6e628eb93f3852dcca56193b3c3db0
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-06 13:22:11 +0100

    l'ajout de la route du dashborad en sebasant sur le controleur de cette dernière

--- Ra-Track/routes/api.php
+++ Ra-Track/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlaneController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/dashboard', [DashboardController::class, 'showDashboard'])->name('dashboard');
+
+Route::get('/planes', [PlaneController::class, 'index']);
+Route::get('/planes/{id}', [PlaneController::class, 'show']);
+Route::post('/planes', [PlaneController::class, 'store']);
+Route::put('/planes/{id}', [PlaneController::class, 'update']);
+Route::delete('/planes/{id}', [PlaneController::class, 'destroy']);
--- Ra-Track/routes/web.php
+++ Ra-Track/routes/web.php
@@ -50,6 +50,4 @@ Route::get('/pdfReceipt', function () {
     return view('pdfReceipt');
 });
 
-Route::get('/dashboardAdmin', function () {
-    return view('dashboardAdmin');
-});
+
