6a98e0472a37d692cbd2b3728a4c861ce8251a8d
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-06 13:19:08 +0100

    implémentation de la fonction showDashboard dans le controleur des dashboard

--- Ra-Track/app/Http/Controllers/DashboardController.php
+++ Ra-Track/app/Http/Controllers/DashboardController.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-   
+    public function showDashboard()
+    {
+        // Récupérer tous les avions de la base de données
+        $planes = Plane::all(); // Récupère tous les avions
+    
+        // Retourner la vue avec les données des avions
+        return view('dashboardAdmin', compact('planes')); // Passe la variable à la vue
+    }
 }
 
 
