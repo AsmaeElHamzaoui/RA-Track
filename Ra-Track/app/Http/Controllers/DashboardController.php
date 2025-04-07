37a40bb08f7af1f6fe7635fb2fec6f9c2eaf190c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-07 01:06:11 +0100

    passer la variable airports au paramètre de la vue dashboard

--- Ra-Track/app/Http/Controllers/DashboardController.php
+++ Ra-Track/app/Http/Controllers/DashboardController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Plane; // Ajoute cette ligne pour importer le modèle Plane
+use App\Models\Plane; 
+use App\Models\Airport;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,9 +12,10 @@ class DashboardController extends Controller
     {
         // Récupérer tous les avions de la base de données
         $planes = Plane::all(); // Récupère tous les avions
-    
+       
+        $airports = Airport::all(); // Récupère tous les aéroports
         // Retourner la vue avec les données des avions
-        return view('dashboardAdmin', compact('planes')); // Passe la variable à la vue
+        return view('dashboardAdmin', compact('planes','airports')); // Passe la variable à la vue
     }
 }
 
