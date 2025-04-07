495c1782b82f54df4438ad5ab3642d178b4caf79
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-07 01:02:19 +0100

    l'ajout des routes des airports

--- Ra-Track/routes/api.php
+++ Ra-Track/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaneController;
+use App\Http\Controllers\AirportController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +22,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-
+// routes planes
 Route::get('/planes', [PlaneController::class, 'index']);
 Route::get('/planes/{id}', [PlaneController::class, 'show']);
 Route::post('/planes', [PlaneController::class, 'store']);
 Route::put('/planes/{id}', [PlaneController::class, 'update']);
 Route::delete('/planes/{id}', [PlaneController::class, 'destroy']);
+
+//routes airports
+Route::get('/airports', [AirportController::class, 'index']);
+Route::get('/airports/{id}', [AirportController::class, 'show']);
+Route::post('/airports', [AirportController::class, 'store']);
+Route::put('/airports/{id}', [AirportController::class, 'update']);
+Route::delete('/airports/{id}', [AirportController::class, 'destroy']);
+
