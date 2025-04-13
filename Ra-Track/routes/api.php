b3a82dad6476fbac6e66d796a610777907243e23
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-13 20:13:13 +0100

    ajout des routes du vol

--- Ra-Track/routes/api.php
+++ Ra-Track/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaneController;
 use App\Http\Controllers\AirportController;
+use App\Http\Controllers\FlightController;
 
 
 /*
@@ -35,4 +36,11 @@ Route::get('/airports/{id}', [AirportController::class, 'show']);
 Route::post('/airports', [AirportController::class, 'store']);
 Route::put('/airports/{id}', [AirportController::class, 'update']);
 Route::delete('/airports/{id}', [AirportController::class, 'destroy']);
+
+//routes flights
+Route::get('/flights', [FlightController ::class, 'index']);
+Route::get('/flights/{id}', [FlightController ::class, 'show']);
+Route::post('/flights', [FlightController ::class, 'store']);
+Route::put('/flights/{id}', [FlightController ::class, 'update']);
+Route::delete('/flights/{id}', [FlightController ::class, 'destroy']);
 
