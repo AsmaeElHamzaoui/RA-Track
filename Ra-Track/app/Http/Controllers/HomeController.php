00bef99171c6223c8a023f355dfa5137c0e82415
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-14 13:28:45 +0100

    l'implémentation de la fonction showBooking pour la récupération des vols , avions et des aéroports

--- Ra-Track/app/Http/Controllers/HomeController.php
+++ Ra-Track/app/Http/Controllers/HomeController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Flight;
+use App\Models\Plane; 
+use App\Models\Airport;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function showBooking()
+    {
+        $flights = Flight::all(); // Récupère tous les vols
+        $planes = Plane::all(); // Récupère tous les avions
+        $airports = Airport::all(); // Récupère tous les aéroports
+        // Retourner la vue avec les données des avions
+        return view('bookingAirplane', compact('planes','airports','flights')); // Passe la variable à la vue
+    }
 }
