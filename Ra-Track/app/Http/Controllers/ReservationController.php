3eeb5b4a583eeaf6ad26f4fb2983d2b22b2ff4b5
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-15 10:29:18 +0100

    ajout des fonctions show et submit dans le controleur de reservation

--- Ra-Track/app/Http/Controllers/ReservationController.php
+++ Ra-Track/app/Http/Controllers/ReservationController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 class ReservationController extends Controller
 {
    
+    public function show($id)
+    {
+        $flight = Flight::with(['departureAirport', 'arrivalAirport'])->findOrFail($id);
+        return view('reservation.show', compact('flight'));
+    }
 
+    public function submit(Request $request)
+{
+    
+}
 }
