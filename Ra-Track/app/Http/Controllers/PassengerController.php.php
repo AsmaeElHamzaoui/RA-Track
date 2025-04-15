64cef54d450075fa29de4c491ae59dd4831951f2
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-15 15:46:04 +0100

    implémentation de la fonction index dans le controleur de passenger

--- Ra-Track/app/Http/Controllers/PassengerController.php.php
+++ Ra-Track/app/Http/Controllers/PassengerController.php.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Passenger;
 use Illuminate\Http\Request;
 
-class PassengerController.php extends Controller
+class PassengerController extends Controller
 {
-    //
+    /**
+     * Affiche la liste de tous les passagers.
+     */
+    public function index()
+    {
+        $passengers = Passenger::all();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $passengers
+        ]);
+    }
+
+   
 }
