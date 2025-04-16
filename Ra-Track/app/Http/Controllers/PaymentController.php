1eb82255f096bf3fb739e95b24cab00e63979b2e
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-16 17:08:31 +0100

    la création du payment controleur

---
+++ Ra-Track/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Flight;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+
+class PaymentController extends Controller
+{
+
+}
