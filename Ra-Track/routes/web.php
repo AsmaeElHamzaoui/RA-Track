323e10f54a8978d68f63a679bf5d34c3be70d4d4
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-19 12:07:48 +0100

    modification de la route get du payment

--- Ra-Track/routes/web.php
+++ Ra-Track/routes/web.php
@@ -66,11 +66,14 @@ Route::get('/pdfReceipt', function () {
 
 
 
-
-Route::get('/payments', [PaymentController::class, 'index'])->name('payments.index');
+Route::get('/payments/{reservation}', [PaymentController::class, 'index'])->name('payments.index')->middleware('auth');
 Route::get('/payments/create', [PaymentController::class, 'create'])->name('payments.create');
 Route::post('/payments', [PaymentController::class, 'store'])->name('payments.store');
-Route::get('/payments/{id}', [PaymentController::class, 'show'])->name('payments.show');
 Route::get('/payments/{id}/edit', [PaymentController::class, 'edit'])->name('payments.edit');
 Route::put('/payments/{id}', [PaymentController::class, 'update'])->name('payments.update');
 Route::delete('/payments/{id}', [PaymentController::class, 'destroy'])->name('payments.destroy');
+
+Route::post('/checkout/{reservation}', [PaymentController::class, 'checkout'])->name('checkout');
+
+Route::get('/payment-success/{id}', [PaymentController::class, 'success'])->name('payment.success');
+Route::get('/payment-cancel', [PaymentController::class, 'cancel'])->name('payment.cancel');
