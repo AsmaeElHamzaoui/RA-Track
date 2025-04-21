6cab529060e6a75a351642210774d348729c5490
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-21 16:22:41 +0100

    Récupération des utilisateurs dans le controleur dashboard

--- Ra-Track/app/Http/Controllers/DashboardController.php
+++ Ra-Track/app/Http/Controllers/DashboardController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Flight;
 use App\Models\Plane; 
 use App\Models\Airport;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function showDashboard()
+
     {
+        $users = User::all(); // Récupère tous les utilisateurs
         $flights = Flight::all(); // Récupère tous les vols
         $planes = Plane::all(); // Récupère tous les avions
         $airports = Airport::all(); // Récupère tous les aéroports
         // Retourner la vue avec les données des avions
-        return view('dashboardAdmin', compact('planes','airports','flights')); // Passe la variable à la vue
+        return view('dashboardAdmin', compact('planes','airports','flights','users')); // Passe la variable à la vue
     }
 }
 
