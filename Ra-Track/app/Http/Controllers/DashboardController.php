0b1e9bf63afb606728c99236f372e557109db708
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-26 18:57:59 +0100

    passer les données de la réservation à la vue dashboardadmin

--- Ra-Track/app/Http/Controllers/DashboardController.php
+++ Ra-Track/app/Http/Controllers/DashboardController.php
@@ -47,11 +47,11 @@ class DashboardController extends Controller
         : 'Aucun';
 
     // Retour de la vue avec toutes les données
-    // return view('dashboardAdmin', compact(
-    //     'planes', 'airports', 'flights', 'users',
-    //     'totalFlights', 'totalPlanes', 'statusDistribution',
-    //     'monthlyFlights', 'activeAirportName', 'reservations'
-    // ));    
+    return view('dashboardAdmin', compact(
+        'planes', 'airports', 'flights', 'users',
+        'totalFlights', 'totalPlanes', 'statusDistribution',
+        'monthlyFlights', 'activeAirportName', 'reservations'
+    ));    
     }
 }
 
