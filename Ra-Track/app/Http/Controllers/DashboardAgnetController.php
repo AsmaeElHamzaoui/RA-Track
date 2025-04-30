42e3861693696b5557db2f04e812e39a484667bc
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-30 16:50:24 +0100

    la création de la fonction showDashboard dans le controleur d'agent

---
+++ Ra-Track/app/Http/Controllers/DashboardAgnetController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Flight;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Models\FlightReport;
+
+
+class DashboardPilotController extends Controller
+{
+    public function showDashboard()
+
+    {
+        
+    }
+}
