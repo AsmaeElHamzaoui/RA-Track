f434b6075298b62224dd13a82df306de59d59fcb
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-30 11:56:35 +0100

    Modifie la requête pour récupérer les rapports de l'utilisateur connecté via la relation avec le vol

--- Ra-Track/app/Http/Controllers/DashboardPilotController.php
+++ Ra-Track/app/Http/Controllers/DashboardPilotController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
-use App\Models\FlightReport; 
+use App\Models\FlightReport;
 
 
 class DashboardPilotController extends Controller
@@ -15,17 +15,16 @@ class DashboardPilotController extends Controller
     public function showDashboard()
 
     {
-       $pilotId = Auth::id(); // ou Auth::user()->id
-       // récupèration des vols assignés au pilote connecté
-       $flights = Flight::where('pilot_id', $pilotId)
-       ->get(['id', 'flight_number']);
+        $pilotId = Auth::id(); // ou Auth::user()->id
+        // récupèration des vols assignés au pilote connecté
+        $flights = Flight::where('pilot_id', $pilotId)
+            ->get(['id', 'flight_number']);
 
-       $reports = FlightReport::with('flight')->latest()->get();
- 
-       return view('dashboardPilot', compact('flights', 'reports')); 
+        // Récupérer les rapports dont le vol appartient au pilote connecté
+        $reports = FlightReport::whereHas('flight', function ($query) use ($pilotId) {
+            $query->where('pilot_id', $pilotId);
+        })->with('flight')->latest()->get();
 
+        return view('dashboardPilot', compact('flights', 'reports'));
     }
 }
-
-
-
