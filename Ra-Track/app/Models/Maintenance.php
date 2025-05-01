4cdc07c60224276495b8f7f510759a8c790bda45
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-05-01 18:44:26 +0100

    l'ajout de fillable au model de maintenance

--- Ra-Track/app/Models/Maintenance.php
+++ Ra-Track/app/Models/Maintenance.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Maintenance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'aircraft_id',
+        'maintenance_type',
+        'start_date',
+        'end_date',
+    ];
 }
