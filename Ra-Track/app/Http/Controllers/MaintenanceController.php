a912b084e4a31edfb36993b4f8ac9a131ad52746
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-05-01 18:56:20 +0100

    l'implémentation de la fonction index pour l'affichage des maintenaces

--- Ra-Track/app/Http/Controllers/MaintenanceController.php
+++ Ra-Track/app/Http/Controllers/MaintenanceController.php
@@ -2,63 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Maintenance;
+use App\Models\Plane;
 use Illuminate\Http\Request;
 
 class MaintenanceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Afficher toutes les maintenances
     public function index()
     {
-        //
+        $maintenances = Maintenance::with('aircraft')->latest()->get();
+        return view('maintenances.index', compact('maintenances'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    
 }
