43d2130b6ab11b6cbb0b5c358901a74bf3010c02
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-05-05 10:32:03 +0100

    modification de la couleur des icons de dashboardAdmin

--- Ra-Track/resources/views/dashboardAdmin.blade.php
+++ Ra-Track/resources/views/dashboardAdmin.blade.php
@@ -130,7 +130,7 @@
                                     5px 5px 15px rgba(0, 0, 0, 0.35),
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
                                 </svg>
                                 <span>Suivi en Direct</span>
@@ -144,7 +144,7 @@
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
                                 <!-- Icône Aéroport (Exemple: Bâtiment ou Tour) -->
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"  style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M8.25 21h7.5M12 3v18m0 0v-6.75m0 6.75H9.75m2.25 0h2.25m-2.25 0V12m0 9V9.75M12 9.75h-2.25m2.25 0h2.25M12 9.75V6m0 3.75v-1.5m0 1.5V3" />
                                 </svg>
                                 <span>Gestion des Aéroports</span>
@@ -157,7 +157,7 @@
                                     5px 5px 15px rgba(0, 0, 0, 0.35),
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 transform -rotate-45">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 transform -rotate-45"  style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                                 </svg>
                                 <span>Gestion des Vols</span>
@@ -171,7 +171,7 @@
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
                                 <!-- Icône Avion (peut être changée si souhaité) -->
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"  style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h17.5" />
                                 </svg>
                                 <span>Gestion des Avions</span>
@@ -184,7 +184,7 @@
                                     5px 5px 15px rgba(0, 0, 0, 0.35),
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                 </svg>
                                 <span>Utilisateurs</span>
@@ -197,7 +197,7 @@
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
                                 <!-- Icône Calendrier (Exemple) -->
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0h18M12 12.75h.008v.008H12v-.008Z" />
                                 </svg>
                                 <span>Réservations</span>
@@ -210,7 +210,7 @@
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
                                 <!-- Icône Calendrier (Exemple) -->
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0h18M12 12.75h.008v.008H12v-.008Z" />
                                 </svg>
                                 <span>Payments</span>
@@ -222,7 +222,7 @@
                                     5px 5px 15px rgba(0, 0, 0, 0.35),
                                     inset -5px -5px 15px rgba(255, 255, 255, 0.1),
                                     inset 5px 5px 15px rgba(0, 0, 0, 0.35);">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5" style="color:#FFD476">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75c0 .621-.504 1.125-1.125 1.125h-2.25A1.125 1.125 0 0 1 3 21v-7.875ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
                                 </svg>
                                 <span>Statistiques</span>
@@ -241,7 +241,7 @@
                 <div></div>
                 <div class="flex items-center space-x-4 mt-4 md:mt-0">
                     <button class="burger-menu md:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="color:#FFD476">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                     </button>
