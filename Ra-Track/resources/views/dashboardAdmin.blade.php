592e949bf901780f598a9fe355f55f358b97ff30
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-05-05 10:16:10 +0100

    l'ajout du logo à la page dahboard Admin

--- Ra-Track/resources/views/dashboardAdmin.blade.php
+++ Ra-Track/resources/views/dashboardAdmin.blade.php
@@ -119,10 +119,7 @@
         <aside class="w-64 flex-shrink-0 p-4 flex flex-col justify-between" style="background-color:rgb(22, 34, 56); border-right: 1px solid #ccc;">
             <div>
                 <div class="flex items-center space-x-2 p-2 mb-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-blue-400 transform -rotate-45">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                    </svg>
-                    <h1 class="text-xl font-bold">AirControl</h1>
+                  <img src="{{ asset('images/logo.png') }}" alt="LimoWide Logo" class="h-6 md:h-8">
                 </div>
 
                 <nav>
