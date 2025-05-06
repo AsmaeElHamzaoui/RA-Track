a7d7e16654bf46415f0130792f9cb54ca55b13b3
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-05-06 04:15:56 +0100

    L'implémentation de la fonction createPlaneIcon dans le composant dashboard

--- Ra-Track/resources/views/components/Dashboard.blade.php
+++ Ra-Track/resources/views/components/Dashboard.blade.php
@@ -171,7 +171,18 @@ document.addEventListener('DOMContentLoaded', function() {
          }
     }
 
-   
+    // --- Création Icône Avion ---
+    function createPlaneIcon(rotation = 0) {
+        const validRotation = typeof rotation === 'number' && !isNaN(rotation) ? rotation : 0;
+        const iconHtml = `<svg xmlns="http://www.w3.org/2000/svg" class="plane-svg-icon" style="transform: rotate(${validRotation}deg);" viewBox="0 0 24 24" fill="currentColor"><path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/></svg>`;
+        return L.divIcon({
+            html: iconHtml, className: 'dummy-transparent-bg',
+            iconSize: [28, 28],
+            iconAnchor: [14, 14]
+        });
+    }
+
+    
 
     // --- Lancement ---
     loadAndStartSimulation();
